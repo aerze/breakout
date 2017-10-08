01a7ec5329ba663af69619f4c127391dca6ccab2
= <gus@mythril.co>
2017-10-08 09:13:15 -0700

    adds paddle with controls

--- src/state.js
+++ src/state.js
@@ -4,13 +4,17 @@ import game from './main'
 const mainState = {
   preload () {
     game.load.image('paddle', 'assets/paddle.png')
+<<<<<<< 29db218457f6382d280b8d4e0633925a088006c7
     game.load.image('brick', 'assets/box.png')
+=======
+>>>>>>> adds paddle with controls
   },
 
   create () {
     game.stage.backgroundColor = '#3598db'
     game.physics.startSystem(Phaser.Physics.ARCADE)
     game.world.enableBody = true
+<<<<<<< 29db218457f6382d280b8d4e0633925a088006c7
 
     this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
     this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
@@ -29,6 +33,14 @@ const mainState = {
         this.bricks.add(brick)
       }
     }
+=======
+
+    this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+    this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+
+    this.paddle = game.add.sprite(200, 400, 'paddle')
+    this.paddle.body.immovable = true
+>>>>>>> adds paddle with controls
   },
 
   update () {
