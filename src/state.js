bf41c14e1b135d5b5bb53c3ab12b29b2942c6adf
= <gus@mythril.co>
2017-10-08 09:29:31 -0700

    add ball

--- src/state.js
+++ src/state.js
@@ -4,21 +4,14 @@ import game from './main'
 const mainState = {
   preload () {
     game.load.image('paddle', 'assets/paddle.png')
-<<<<<<< 01a7ec5329ba663af69619f4c127391dca6ccab2
-<<<<<<< 29db218457f6382d280b8d4e0633925a088006c7
     game.load.image('brick', 'assets/box.png')
-=======
->>>>>>> adds paddle with controls
-=======
-    game.load.image('brick', 'assets/box.png')
->>>>>>> add bricks
+    game.load.image('ball', 'assets/ball.png')
   },
 
   create () {
     game.stage.backgroundColor = '#3598db'
     game.physics.startSystem(Phaser.Physics.ARCADE)
     game.world.enableBody = true
-<<<<<<< 29db218457f6382d280b8d4e0633925a088006c7
 
     this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
     this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
@@ -37,17 +30,14 @@ const mainState = {
         this.bricks.add(brick)
       }
     }
-<<<<<<< 01a7ec5329ba663af69619f4c127391dca6ccab2
-=======
 
-    this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
-    this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+    this.ball = game.add.sprite(200, 200, 'ball')
 
-    this.paddle = game.add.sprite(200, 400, 'paddle')
-    this.paddle.body.immovable = true
->>>>>>> adds paddle with controls
-=======
->>>>>>> add bricks
+    this.ball.body.velocity.x = 200
+    this.ball.body.velocity.y = 200
+
+    this.ball.body.bounce.setTo(1)
+    this.ball.body.collideWorldBounds = true
   },
 
   update () {
