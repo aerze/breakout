614b1638dd2bc06056abe39148bd0a0ebb942f39
= <gus@mythril.co>
2017-10-08 09:13:15 -0700

    adds paddle with controls

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ import mainState from './state'
 const game = new Phaser.Game(400, 450)
 game.state.add('main', mainState)
 game.state.start('main')
+
+export default game
--- src/state.js
+++ src/state.js
@@ -1,14 +1,27 @@
+import Phaser from 'phaser-ce'
+import game from './main'
+
 const mainState = {
   preload () {
-
+    game.load.image('paddle', 'assets/paddle.png')
   },
 
   create () {
+    game.stage.backgroundColor = '#3598db'
+    game.physics.startSystem(Phaser.Physics.ARCADE)
+    game.world.enableBody = true
 
+    this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+    this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+
+    this.paddle = game.add.sprite(200, 400, 'paddle')
+    this.paddle.body.immovable = true
   },
 
   update () {
-
+    if (this.left.isDown) this.paddle.body.velocity.x = -300
+    else if (this.right.isDown) this.paddle.body.velocity.x = 300
+    else this.paddle.body.velocity.x = 0
   }
 }
 
